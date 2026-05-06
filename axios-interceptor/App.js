9dfbc9a5b443b0e24bed9cc3ed267e70f31949c4
prathmesh joshi <147234424+prathmesh1421@users.noreply.github.com>
2026-05-06 17:22:38 +0530

    Create App.js

---
+++ axios-interceptor/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PatientDashboard from "./PatientDashboard";
+
+function App() {
+  return (
+    <div>
+      <PatientDashboard />
+    </div>
+  );
+}
+
+export default App;
